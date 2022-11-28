4a7dbf75e73dd4c439a9c279c79cebd3973d372c
antonio <caaz160782@gmail.com>
2022-11-28 17:34:18 -0600

    end login with google

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const User = require('../models/user'); 
 const bcrypt = require('../helpers/bcrypt');
 const jwt = require("../helpers/jwt");
+const {googleVerify} =require("../helpers/google-verify")
 
 const login = async (req, res) => {
   const { email, password } = req.body;
@@ -43,6 +44,55 @@ const login = async (req, res) => {
  
 }
 
+const googleSignIn = async (req, res) => {
+  const { id_token } = req.body;
+
+  try {
+    const { name, imagen,email } =await googleVerify(id_token);
+    //const googleUser=await googleVerify(id_token);
+    let user = await User.findOne({ email });
+    if (!user) {
+      const data = {
+        name,
+        imagen,
+        email,
+        password: ':P',
+        google: true,
+        rol:"ADMIN_ROL"
+      };
+      user = new User(data);
+      await user.save();
+    }
+
+    if (!user.state) {
+      return res.status(400).json({
+      msg: 'Hable con el admin, user bloqueado'
+     });   
+    }
+
+    const payload = {
+                       sub:user._id.toString(),                         
+                       rol:user.rol, 
+                     }  
+    const token = await jwt.token(payload)    
+
+
+    res.status(200).json({
+      token,
+      email,
+    msg: 'todo bien'
+    });  
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({
+      msg: 'El token no se pudo verificar'
+    }); 
+  }
+
+  
+  
+}
+
 module.exports = {
-login
+login,googleSignIn
 }
